5505680fe321790654dd9c41e731430955858ff9
rklats <100431328+rklats@users.noreply.github.com>
2022-03-17 12:17:38 +0100

    Create __version__.py

---
+++ pywebcopy/__version__.py
@@ -0,0 +1,9 @@
+# Copyright 2020; Raja Tomar
+# See license for more details
+__title__ = 'pywebcopy'
+__url__ = 'https://github.com/rklats/pywebcopy/'
+__author__ = 'Raja Tomar'
+__email__ = 'rajatomar788@gmail.com'
+__license__ = 'Apache Software License'
+__version__ = '33f8e808bfb3e4816314357db2f5f0e9d384f2fe-rklats'
+__description__ = 'Python library to clone/archive pages or sites from the Internet.'
